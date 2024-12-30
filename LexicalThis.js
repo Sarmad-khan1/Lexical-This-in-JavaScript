cb260eb1d009b6c73cf6873122a5e6a62b084f87
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-12-30 15:48:45 +0500

    Add files via upload

---
+++ LexicalThis.js
@@ -0,0 +1,9 @@
+<script>
+        function Person(firstName, lastName, dob){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.dob = dob;
+        }
+        const person1 = new Person('Sarmad','AJ','27-04-2000');
+        console.log(person1);
+</script>
